7aaa2437585a8340503ed415406f845ceedd8afa
Nguyen The Hoai Nam <76147564+nthnam17@users.noreply.github.com>
2024-03-13 22:43:18 +0700

    @@

---
+++ chillandfree-api/app/Models/song.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class song extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name','singer_id','producer','replay','album_id','like'
+    ];
+
+    protected $table = 'song';
+}
---
+++ chillandfree-api/database/migrations/2024_03_13_145637_create_song_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSongTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('song', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->integer('singer_id');
+            $table->string('producer');
+            $table->string('like');
+            $table->string('replay');
+            $table->integer('album_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('song');
+    }
+}
